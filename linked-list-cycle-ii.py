40d2810498b466d62782e057a318d06836159a95
vincy singh <vincysjsu@gmail.com>
2024-07-30 22:05:51 -0700

    Linked-List-1 Done

---
+++ linked-list-cycle-ii.py
@@ -0,0 +1,28 @@
+#Time complexity O(n)
+#Space complexity O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None: return None
+
+        slow = fast = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                break
+        else:
+            return None
+        
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+            
